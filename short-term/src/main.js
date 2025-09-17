4ea493fd4ceb972c9387238e83c40ff820775245
SongJiaGang-del <3574171434@qq.com>
2025-09-17 10:36:36 +0800

    Update main.js

--- short-term/src/main.js
+++ short-term/src/main.js
@@ -155,7 +155,7 @@ function loadAstronaut() {
   document.getElementById("app").appendChild(loadingDiv);
 
   loader.load(
-    "/model/Astronaut/scene.gltf",
+    "./model/Astronaut/scene.gltf",
     (gltf) => {
       astronaut = gltf.scene;
       astronaut.scale.set(1, 1, 1);
