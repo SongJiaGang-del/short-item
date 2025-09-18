d9508ebafe5ce8ca695719dc9e4cab8504acf28c
SongJiaGang-del <3574171434@qq.com>
2025-09-18 10:48:18 +0800

    Update vite.config.js

--- short-term/vite.config.js
+++ short-term/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/short-item/" : "/",
   server: {
     port: 3001,
     host: true,
